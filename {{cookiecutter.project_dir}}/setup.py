5523a983d3f0b3a6c1d8492c6ad7142ab4a57faf
Willem Meints <1550763+wmeints@users.noreply.github.com>
2020-11-02 19:44:53 +0100

    Create setup.py

---
+++ {{cookiecutter.project_dir}}/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst', 'r') as readme_file:
+    readme_content = readme_file.read()
+
+with open('LICENSE', 'r') as license_file:
+    license_content = license_file.read()
+
+setup(
+    name='{{cookiecutter.package_name}}',
+    version='0.1.0',
+    description='{{cookiecutter.project_name}}',
+    long_description=readme_content,
+    license=license_content,
+    author='{{cookiecutter.author_name}}',
+    author_email='{{cookiecutter.author_email}}',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'tensorflow>=2.3'
+    ]
+)
